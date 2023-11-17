169ba18e4e0aad744672f5fdc517be7646b387b4
Dimitar Mitev <90791969+nfsmitko@users.noreply.github.com>
2023-11-17 18:26:40 +0200

    Create database

---
+++ Source/Obqva.Store/AdDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Obqva.Domain.Entities;
+
+namespace Obqva.Store
+{
+    public class AdDbContext: DbContext
+    {
+        public AdDbContext(DbContextOptions options) : base(options) { }
+
+        public virtual DbSet<Ad> Ad => Set<Ad>();
+    }
+}
--- Source/Obqva/Program.cs
+++ Source/Obqva/Program.cs
@@ -1,8 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using Obqva.Store;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("ObqvaDbContext");
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AdDbContext>(options => options.UseNpgsql(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
